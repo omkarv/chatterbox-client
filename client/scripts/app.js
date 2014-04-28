911f159a280cd1e12d013881392b86355f2ae54c
shall <hall.steven.g@gmail.com>
2014-04-28 13:22:13 -0700

    basic ajax calls

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,44 @@
-// YOUR CODE HERE:
+var APP = {
+  Store: function () {
+    this._store = [];
+  },
+  Message: function (userName, roomName, text) {
+    this.user = userName;
+    this.room = roomName;
+    this.text = text;
+  }
+}
 
+APP.Store.prototype.update = function () {
+  var that = this;
+  $.ajax({
+    url : 'https://api.parse.com/1/classes/chatterbox',
+    type : "GET",
+    cache : false,
+    accept: '*/*',
+    connection: 'keep-alive',
+    success : function(res) {
+      res.results.forEach(function (msg) {
+        that._store.push(msg);
+      });
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+APP.Message.prototype.send = function () {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(this),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+}
